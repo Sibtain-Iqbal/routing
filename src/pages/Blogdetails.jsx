efd7726a140d2345f56491592f21a1cfd1ff52a7
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-05-16 22:50:59 -0700

    routing

--- src/pages/Blogdetails.jsx
+++ src/pages/Blogdetails.jsx
@@ -9,10 +9,14 @@ let currentData = blogs.filter((v,i)=> v.id == currentid)[0]
 console.log(currentData);
 
   return (
-    <div>
+    <div className='component'>
        <Header/>  
+       <div className='inner'>
        <h1>{currentData.id}</h1>
-       <h1>{currentData.title}</h1>
+       <h2>{currentData.title}</h2>
+       <p>{currentData.body}</p>
+       </div>
+       
       
     </div>
   )
